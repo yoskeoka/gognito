cc1f98e3f769fd8408b33f8036a49d3b2dc6ff08
gcoka <y.okamoto@gemcook.com>
2018-03-19 12:38:16 +0900

    [update] cognito setting in localStorage

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,13 +4,15 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 
 import WithAuthRoutes from "./WithAuthRoutes";
-import IDPCallback from "./containers/IDPCallback"
+import IDPCallback from "./containers/IDPCallback";
+import CognitoClientSetting from "./containers/CognitoClientSetting";
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/a" component={WithAuthRoutes} />
     <Route path="/idpcallback" component={IDPCallback} />
+    <Route path="/cognitosetting" component={CognitoClientSetting} />
     <Route component={NotFound} />
   </Switch>
 );
--- src/awsConfig.js
+++ src/awsConfig.js
@@ -1,36 +1,46 @@
-import { CognitoAuth } from 'amazon-cognito-auth-js/dist/amazon-cognito-auth';
+import { CognitoAuth } from "amazon-cognito-auth-js/dist/amazon-cognito-auth";
 export const AuthConfig = {
   // identityPoolId はCognito Identity PoolのFederatedItentityProviderにGoogleやFacebookを登録している場合に必要。
   // このサンプルではCognito User PoolのユーザーとしてGoogleやFacebookのユーザーをログインさせる(UserPoolのログインIDが付与される)
   // identityPoolId: "us-east-1:6317760e-ae25-4c71-9901-855dd1d9c435", //REQUIRED - Amazon Cognito Identity Pool ID
-  region: "us-east-1", // REQUIRED - Amazon Cognito Region
-  userPoolId: "us-east-1_VpFb4U9MM", //OPTIONAL - Amazon Cognito User Pool ID
-  userPoolWebClientId: "21mafgufsmc9m94kp7jlqemp0d" //OPTIONAL - Amazon Cognito Web Client ID
+  region: localStorage.getItem("region") || "", // REQUIRED - Amazon Cognito Region
+  userPoolId: localStorage.getItem("userPoolId") || "", //OPTIONAL - Amazon Cognito User Pool ID
+  userPoolWebClientId: localStorage.getItem("appClientId") || "" //OPTIONAL - Amazon Cognito Web Client ID
 };
 
-export const GetCognitoAuth = (identifyProvider, onSuccess, onFailure)=>{
-
+export const GetCognitoAuth = (identifyProvider, onSuccess, onFailure) => {
   var authData = {
-    ClientId : '21mafgufsmc9m94kp7jlqemp0d', // Your client id here
-    AppWebDomain : 'fs-fish-test.auth.us-east-1.amazoncognito.com',
-    TokenScopesArray : ['profile', 'email', 'openid', 'aws.cognito.signin.user.admin', 'phone'],
-    RedirectUriSignIn : 'http://localhost:3000/idpcallback',
-    RedirectUriSignOut : 'http://localhost:3000/',
+    ClientId: localStorage.getItem("appClientId") || "", // Your client id here
+    AppWebDomain: `${localStorage.getItem(
+      "appClientDomain"
+    )}.auth.${localStorage.getItem("region")}.amazoncognito.com`,
+    TokenScopesArray: [
+      "profile",
+      "email",
+      "openid",
+      "aws.cognito.signin.user.admin",
+      "phone"
+    ],
+    RedirectUriSignIn: "http://localhost:3000/idpcallback",
+    RedirectUriSignOut: "http://localhost:3000/",
     IdentityProvider: identifyProvider
   };
   var auth = new CognitoAuth(authData);
   auth.userhandler = {
     onSuccess: function(result) {
-      if (onSuccess){onSuccess(result);}
+      if (onSuccess) {
+        onSuccess(result);
+      }
       console.log(result);
     },
     onFailure: function(err) {
       console.log(err);
-      if (onFailure){onFailure(err);}
+      if (onFailure) {
+        onFailure(err);
+      }
     }
   };
   auth.useCodeGrantFlow();
 
   return auth;
-
 };
